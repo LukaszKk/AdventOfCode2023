6bcb49af93d3adbcfd3d3206ff89918732d8748d
Łukasz Kuk <lukasz010420@gmail.com>
2023-12-14 17:04:50 +0100

    day14 part1 init

---
+++ day14/part1.py
@@ -0,0 +1,26 @@
+import os
+
+input_file = "input.txt"
+
+
+def calculate(lines: list[str]) -> int:
+    res = 0
+    for line in lines:
+        pass
+    return res
+
+
+def read_input() -> list[str]:
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{current_dir}/{input_file}", "r") as f:
+        return f.readlines()
+
+
+def main():
+    lines = read_input()
+    output = calculate(lines)
+    print(f"Result: {output}")
+
+
+if __name__ == "__main__":
+    main()
